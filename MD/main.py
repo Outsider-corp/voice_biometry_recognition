f924be7a499d42c02250a3b2ec8f0896dde06aeb
Insider <romikfedkov@gmail.com>
2024-04-15 00:28:22 +0300

    add mfcc(1)

--- MD/main.py
+++ MD/main.py
@@ -1,6 +1,8 @@
 import os
 from typing import List
 
+import librosa
+from librosa import feature
 import torch
 import torch.nn as nn
 import numpy as np
@@ -26,7 +28,14 @@ def get_audio_for_id(folder: str, id: str) -> List[str]:
 id_list = os.listdir(data_path)
 
 # Получение голосовых признаков
-voice_params = []
+voice_params = {}
 
 for person_id in id_list:
     files = get_audio_for_id(data_path, person_id)
+    person_params = []
+    for file in files:
+        audio, sample_rate = librosa.load(file, sr=None)
+
+        mfcc = feature.mfcc(y=audio, sr=sample_rate, n_mfcc=13)
+        person_params.append(mfcc)
+    voice_params[person_id] = person_params
